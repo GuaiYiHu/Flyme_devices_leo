c0a8a1f3b038a29034ada5316505671a97b6ceb1
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-11 17:40:12 +0800

    auto copy recovery and config
    Change-Id: If694138381ebdea5665000cbf02ae4f334c4a38f

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -3,3 +3,7 @@
 TARGET_DIR=out/merged_target_files
 
 cp -rf other/UPDATE-SuperSU.zip $TARGET_DIR/META/
+
+cp -rf other/flash.config $TARGET_DIR/META/
+
+cp -rf other/twrp_recovery.img $TARGET_DIR/META/
--- releasetools.py
+++ releasetools.py
@@ -13,6 +13,14 @@ ui_print("* Powered by GuaiYiHu");
 ui_print("* Now loading Flyme...");
 ui_print("******************************************");\n''' + edify.script[0]
 
+def CopyConfig(info):
+    config = info.input_zip.read("META/flash.config")
+    common.ZipWriteStr(info.output_zip, "flash.config", config)
+
+def CopyRecovery(info):
+    recovery = info.input_zip.read("META/twrp_recovery.img")
+    common.ZipWriteStr(info.output_zip, "twrp_recovery.img", recovery)
+
 def InstallSuperSU(info):
     root = info.input_zip.read("META/UPDATE-SuperSU.zip")
     common.ZipWriteStr(info.output_zip, "SuperSU/UPDATE-SuperSU.zip", root)
@@ -28,4 +36,6 @@ def FullOTA_InstallEnd(info):
     edify = info.script
     ModifyBegin(edify)
     InstallSuperSU(info)
+    CopyConfig(info)
+    CopyRecovery(info)
     FlashSUperSU(info)
