dfec47ab1eb76b87d5c64afb1034a3877bd1e6ca
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-12 20:36:49 +0800

    Revert "add root"
    This reverts commit 933472cc824fa01a9f8f318b7c5bf87ec16c59e2.
    
    Change-Id: I4239591af92f363f76be63f6133f8664658d3dae

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -2,8 +2,6 @@
 
 TARGET_DIR=out/merged_target_files
 
-cp -rf other/UPDATE-SuperSU.zip $TARGET_DIR/META/
-
 cp -rf other/flash.config $TARGET_DIR/META/
 
 cp -rf other/twrp_recovery.img $TARGET_DIR/META/
--- releasetools.py
+++ releasetools.py
@@ -21,21 +21,8 @@ def CopyRecovery(info):
     recovery = info.input_zip.read("META/twrp_recovery.img")
     common.ZipWriteStr(info.output_zip, "twrp_recovery.img", recovery)
 
-def InstallSuperSU(info):
-    root = info.input_zip.read("META/UPDATE-SuperSU.zip")
-    common.ZipWriteStr(info.output_zip, "SuperSU/UPDATE-SuperSU.zip", root)
-
-def FlashSUperSU(info):
-    info.script.AppendExtra(('ui_print("Flashing SuperSU...");'))
-    info.script.AppendExtra(('package_extract_dir("SuperSU", "/tmp/supersu");'))
-    info.script.AppendExtra(('run_program("/sbin/busybox", "unzip", "/tmp/supersu/UPDATE-SuperSU.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");'))
-    info.script.AppendExtra(('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/UPDATE-SuperSU.zip");'))
-    info.script.AppendExtra(('ui_print("Finish!");'))
-
 def FullOTA_InstallEnd(info):
     edify = info.script
     ModifyBegin(edify)
-    InstallSuperSU(info)
     CopyConfig(info)
     CopyRecovery(info)
-    FlashSUperSU(info)
